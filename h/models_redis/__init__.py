585c6cbaad77019fae8718fd384155fbf0f02ced
Ivan Silva <ivan.silvaferaud@uts.edu.au>
2024-01-16 13:58:22 +1100

    fixing issues

--- h/models_redis/__init__.py
+++ h/models_redis/__init__.py
@@ -158,18 +158,18 @@ def fetch_all_user_sessions(userid):
     table_result=[]
     for index, item in enumerate(result):
         json_item = {'id': index, **get_user_event(item.pk)}
-        if not auxSessionIds:
-            table_result.append(json_item)
-            auxSessionIds.append(json_item['doc_id'])
-        else:
-            flag=True
-            for sesionid in auxSessionIds:
-                if sesionid==json_item['doc_id']:
-                    flag=False
-            if flag:
+        if json_item['doc_id'] is not None and json_item['doc_id']!="" and json_item['interaction_context'] is not None and json_item['interaction_context']!="":
+            if not auxSessionIds:
                 table_result.append(json_item)
                 auxSessionIds.append(json_item['doc_id'])
-    print("INIT ", table_result)
+            else:
+                flag=True
+                for sesionid in auxSessionIds:
+                    if sesionid==json_item['doc_id']:
+                        flag=False
+                if flag:
+                    table_result.append(json_item)
+                    auxSessionIds.append(json_item['doc_id'])
       
     return {
         "table_result": table_result,
