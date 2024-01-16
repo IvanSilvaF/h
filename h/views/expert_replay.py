13000ffd67a226ff1941faf76a50303f708c9def
Ivan Silva <ivan.silvaferaud@uts.edu.au>
2024-01-16 14:38:53 +1100

    fixxing issues

--- h/views/expert_replay.py
+++ h/views/expert_replay.py
@@ -5,22 +5,21 @@ import os
 from h.models_redis import fetch_all_user_sessions
 import time
 
-#@view_defaults(route_name="account_expert_replay", renderer="h:templates/accounts/kmass-user-expert-replay.html.jinja2",is_authenticated=True)
-#class ExpertController:
-#    def __init__(self, request):
-#        self.request = request
+class ExpertController:
+    def __init__(self, request):
+        self.request = request
 
-@view_config(route_name="account_expert_replay", request_method="GET", renderer="h:templates/accounts/kmass-user-expert-replay.html.jinja2", is_authenticated=True)
-def getSessionUser(self):
+    @view_config(route_name="account_expert_replay", request_method="GET", renderer="h:templates/accounts/kmass-user-expert-replay.html.jinja2", is_authenticated=True)
+    def getSessionUser(self):
 
-    fetch_result=fetch_all_user_sessions(userid=self.request.authenticated_userid)
+        fetch_result=fetch_all_user_sessions(userid=self.request.authenticated_userid)
 
-    table_results=[]
-    for result in fetch_result["table_result"]:
-        json_item = {'session_id': result['doc_id'], 'task_name': result['interaction_context']}
-        table_results.append(json_item)
-    print("ER ",table_results)
-    return {
-        "table_results": table_results,
-        "zero_message": _("No annotations matched your search."),
-    }
+        table_results=[]
+        for result in fetch_result["table_result"]:
+            json_item = {'session_id': result['doc_id'], 'task_name': result['interaction_context']}
+            table_results.append(json_item)
+        print("ER ",table_results)
+        return {
+            "table_results": table_results,
+            "zero_message": _("No annotations matched your search."),
+        }
