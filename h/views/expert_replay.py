bb350a5ca07645e6b069a07807142cc6e92b4cff
Ivan Silva <ivan.silvaferaud@uts.edu.au>
2024-01-17 11:15:22 +1100

    continue ER

--- h/views/expert_replay.py
+++ h/views/expert_replay.py
@@ -22,4 +22,27 @@ class ExpertController:
         return {
             "table_results": table_results,
             "zero_message": _("No annotations matched your search."),
+            "request": self.request,
+        }
+    @view_config(route_name="process_flow", request_method="GET", renderer="h:templates/accounts/kmass-user-process-flow.html.jinja2", is_authenticated=True)
+    def processFlow(self):
+        print("hola")
+        session_id = self.request.params.get("id")
+        task_name = self.request.params.get("name")
+        print(str(session_id)+" - "+str(task_name))
+        #order = self.request.params.get("order", ORDER)
+        #recevived the tast name and session ID
+        #get all the events 
+        #process the event
+        #Send the result
+        #fetch_result=fetch_all_user_sessions(userid=self.request.authenticated_userid)
+
+        #table_results=[]
+        #for result in fetch_result["table_result"]:
+        #    json_item = {'session_id': result['doc_id'], 'task_name': result['interaction_context']}
+        #    table_results.append(json_item)
+        #print("ER ",table_results)
+        return {
+            "table_results": "PROCCESS FLOW",
+            "zero_message": _("No annotations matched your search."),
         }
